61f9638ab866ba41090c1e93ff610aaf1dea4148
Raime <0961120962fb@gmail.com>
2020-04-21 21:37:14 +0300

    fix

--- BinaryCpp.cpp
+++ BinaryCpp.cpp
@@ -1,46 +1,156 @@
-﻿#include <sys/types.h>             /* включение необходимых заголовочных файлов */ 
+﻿#include <sys/types.h>      /* включение необходимых заголовочных файлов */ 
 #include <fcntl.h> 
 #include <stdlib.h> 
 #include<stdio.h>
 #include <sys\stat.h>
 #include <io.h>
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <windows.h>
+
+using namespace std;
 #pragma warning(disable : 4996)
-#define BUF_SIZE 4096              /* используется буфер размером  4096 байт */ 
-#define OUTPUT_MODE 0700  /* биты защиты для выходного файла */ 
+#define BUF_SIZE 4096               
+void Base();
+void Midle();
+void Hard();
 
 int main(int argc, char *argv[]) 
 {
-      argc = 3;
-    
+    srand(time(0));
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    setlocale(LC_ALL, "");
 
-
-    int in_fd, out_fd, rd_count, wt_count;  
-    char buffer[BUF_SIZE];
-       /* если argc не равен 3, возникает  синтаксическая ошибка */
-
-/* Открытие входного и создание выходного файла */    
-in_fd = open("first.txt", O_RDONLY| O_CREAT | O_WRONLY | O_TEXT );  
-write(in_fd, "sup 2ch", 8);
-/* открытие исходного файла */
-if (in_fd < 0) 
-   // exit(2);                 /* если он не открывается, выйти */ 
-out_fd = creat("second.txt", OUTPUT_MODE);   /* создание файла-приемника */
-if (out_fd < 0)
-    exit(3);                /* если он не создается, выйти */   
-/* Цикл копирования */  
-while (true) {
-    rd_count = read(in_fd, buffer, BUF_SIZE); /* чтение блока данных */  
-    if (rd_count <= 0) 
-        break;      /* в конце файла или при ошибке – выйти из цикла */   
-    wt_count = write(out_fd, buffer, rd_count); /* запись данных */
-        if (wt_count <= 0)
-            exit(4); /* при wt_count <= 0 возникает ошибка */
+    Base();
+   // Midle();
+    //Hard();
 }
-/* Закрытие файлов */   
-close(in_fd);    
-close(out_fd);
-if (rd_count == 0) /* при последнем чтении ошибки не возникло */  
-   exit(0);   
-else
-    exit(5);                  /* ошибка при последнем чтении */
+
+void Base()
+{
+    cout << "\nBase task level\n";
+/*
+Создать бинарный файл, компонентами которого является структура, содержащая следующие поля:  
+ - Наименование товара; 
+ - Стоимость единицы товара; 
+ - Количество каждого товара. 
+Определить общую стоимость товара, предложенного для реализации, и его среднюю цену
+*/
+
+}
+
+void Midle()
+{
+    /*
+Ввести в Memo или в StringGrid некоторое количество вещественных чисел и записать их в бинарный файл.
+Переписать бинарный файл так, чтобы каждое значение, записанное в файле,  было увеличено в 1,5 раза. 
+Распечатать числа до и после изменения данных в файле.
+    */
+    cout << "\Midle task level\n";
+    int countelem = 2 + rand() % 20;
+    const char* file_name = "doubledigitfile";
+    FILE* f_out = fopen(file_name, "wb");
+    double* buffer = new double[countelem] ;
+    for (size_t i = 0; i < countelem; i++)
+    {
+        buffer[i] = - 10 - ( (rand() % 100) / 100.0);
+    }
+    
+    fwrite(buffer, sizeof(double), countelem, f_out);
+    fclose(f_out);
+    cout << "\nData reading in file <<doubledigitfile.bin>>: \n";
+    f_out = fopen(file_name, "rb");
+    fread(buffer, sizeof(double), countelem, f_out);
+    for (size_t i = 0; i < countelem; i++)
+    {
+        cout << buffer[i]<<" ";
+         buffer[i] *= 1.5;
+    }
+    
+    fclose(f_out);
+     f_out = fopen(file_name, "w+b");
+    fwrite(buffer, sizeof(double), countelem, f_out);
+    fclose(f_out);
+    f_out = fopen(file_name, "rb");
+    fread(buffer, sizeof(double), countelem, f_out);
+    cout << "\nData reading in file before manipulation <<doubledigitfile.bin>>: \n";
+
+    for (size_t i = 0; i < countelem; i++)
+    {
+        cout << buffer[i] << " ";
+    }
+
+}
+
+
+struct Student
+{
+     string fio;
+    int marks[3];
+    double grants;//stependia
+    void ShowInfo();
+};
+void Hard()
+{
+    /*
+Создать бинарный файл, компонентами которого является структура, содержащая следующие поля:
+    - фамилия и инициалы студентов;
+    - номер группы; 
+    - успеваемость (массив из трех дисциплин); 
+    - размер стипендии. 
+Переписать бинарный файл так, чтобы для студентов, у которых есть хотя бы одна двойка,  размер стипендии
+должен быть уменьшен на 20%
+    */
+    cout << "\Hard task level\n";
+    
+    Student* masstuds = new Student[3];
+    masstuds[0].fio = "Михайл Сергеевич Грибков";
+    masstuds[1].fio = "Даниил Вениаминович Чабан";
+    masstuds[2].fio = "Ахмед Расим Оглу";
+    for (size_t i = 0; i < 3; i++)
+    {
+        masstuds[i].grants = 1300 + rand() % 601;
+        for (size_t q = 0; q < 3; q++)
+        {
+            masstuds[i].marks[q] = 1 + rand() % 5;
+        }
+    }
+
+    FILE *f_out, *f_in;
+    f_out = fopen("DateBaseStudents", "wb");
+    fwrite(masstuds, sizeof(Student), 3, f_out);
+    fclose(f_out);
+    f_out = fopen("DateBaseStudents", "rb");
+    fread(masstuds, sizeof(Student), 3, f_out);
+    cout << "\nИнформация о студентах считаная с файла <<DateBaseStudents.bin>> \n";
+    for (size_t i = 0; i < 3; i++)
+    {
+        cout << i + 1 << " " << masstuds[i].fio << " " << masstuds[i].grants << " ";
+        for (size_t q = 0; q < 3; q++)
+        {
+            
+            cout << masstuds[i].marks[q]<<" ";
+            if (masstuds[i].marks[q] <= 2)
+                masstuds[i].grants -= masstuds[i].grants * 0.20;
+        }
+        cout << endl;
+    }
+    fclose(f_out);
+    f_out = fopen("DateBaseStudents", "w+b");
+    cout << "\nВнесенеия изменений в базу данных(студентам имеющим хоть одну 2йку размер степендий уменшить на 20%)\n";
+    fwrite(masstuds, sizeof(Student), 3, f_out);
+    for (size_t i = 0; i < 3; i++)
+    {
+        cout << i + 1 << " " << masstuds[i].fio << " " << masstuds[i].grants << " ";
+        for (size_t q = 0; q < 3; q++)
+        {
+
+            cout << masstuds[i].marks[q] << " ";
+            if (masstuds[i].marks[q] == 2)
+                masstuds[i].marks[q] -= masstuds[i].marks[q] * 0.20;
+        }
+        cout << endl;
+    }
 }
